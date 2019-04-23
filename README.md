d13baea78a79c99b7a7144d98f4bf7e51b42766b
Rob Svirskas <svirskasr@janelia.hhmi.org>
2019-04-23 14:48:14 -0400

    Draft working version

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# aquatics-utilities
-Utility programs for Aquatics
+# aquatics-utilities [![Picture](https://raw.github.com/janelia-flyem/janelia-flyem.github.com/master/images/HHMI_Janelia_Color_Alternate_180x40.png)](http://www.janelia.org)
+
+## Utility programs for Aquatics
+
+These applications process exported AMS data to create new tab-delimited files to be loaded into PyRAT
+(that's [PyRAT](https://www.scionics.com/pyrat.html)] the animal facility software, not [Pyrat](http://www.pyratrum.com)] the rum).
+
+## Installation for local use
+
+First, prepare a virtual environment:
+
+1. Create and activate a clean python 3 environment.
+    ```
+    virtualenv -p python3 config_venv --no-site-packages
+    source config_venv/bin/activate
+    ```
+2. Install requirements
+    ```
+    pip3 install -r requirements.txt
+    ```
+
+## Preparation of AMS data
+
+Next, export data from AMS. Export data from User, Stock, and Unit tables:
+
+- User: export all columns into a new tab-delimited file named User.tsv
+- Stock: export StockID, Name, State, and UserID into a new tab-delimited file named Stock.tsv
+- Unit: export Name, State, SetupDate, SetupAmountMales, SetupAmountFemales, CurrentAmountMales, CurrentAmountFemales, FemaleStockID, MaleStockID, and UserID into a new tab-delimited file named Unit.tsv
+
+These files should be in the same directory as the programs.
+
+## Creation of PyRAT files
+
+Run the programs in order:
+
+1. Build the users.txt file:
+   ```
+   python3 export_ams_users.py
+   ```
+2. Build the strains.txt file:
+   ```
+   python3 export_ams_strains.py
+   ```
+3. Build the tanks.txt file:
+   ```
+   python3 export_ams_tanks.py
+   ```
---
+++ export_ams_data.py
@@ -0,0 +1,173 @@
+import argparse
+import sys
+import colorlog
+import requests
+
+# Configuration
+CONFIG = {'config': {'url': 'http://config.int.janelia.org/'}}
+CONN = dict()
+CURSOR = dict()
+
+def call_responder(server, endpoint):
+    url = CONFIG[server]['url'] + endpoint
+    try:
+        req = requests.get(url)
+    except requests.exceptions.RequestException as err:
+        LOGGER.critical(err)
+        sys.exit(-1)
+    if req.status_code in [200, 404]:
+        return req.json()
+    LOGGER.error('Status: %s', str(req.status_code))
+    sys.exit(-1)
+
+
+def process_users():
+    print("Creating users.txt")
+    input = open("User.tsv", "r")
+    output = open("users.txt", "w")
+    output.write("ID\tUsername\tFirst name\tLast name\tEmail\n")
+    for line in input:
+        if line.split("\t")[0] == 'UserID':
+            continue
+        line = line.strip()
+        id = line.split("\t")[0]
+        last = line.split("\t")[1]
+        email = line.split("\t")[2]
+        first = line.split("\t")[7]
+        if email:
+            uid = email.split('@')[0]
+            resp = call_responder('config', 'config/workday/' + uid)
+            if 'config' in resp:
+                output.write("%s\t%s\t%s\t%s\t%s\n" % (id, uid, resp['config']['first'], resp['config']['last'], resp['config']['email']))
+                LOGGER.info(uid)
+                continue
+        if first and last:
+            if first == '(null)':
+                continue
+            uid = last.lower() + first[0].lower()
+            uid = uid.replace('-', '')
+            resp = call_responder('config', 'config/workday/' + uid)
+            if 'config' in resp:
+                output.write("%s\t%s\t%s\t%s\t%s\n" % (id, uid, resp['config']['first'], resp['config']['last'], resp['config']['email']))
+                LOGGER.info(uid)
+                continue
+            else:
+                LOGGER.error("Could not find data for %s", uid)
+                output.write("%s\t%s\t%s\t%s\t%s\n" % (id, uid, first, last, 'CORRECT THIS ROW MANUALLY')) 
+        else:
+            LOGGER.error("Incomplete information for %s", line)
+    input.close()
+    output.close()
+
+
+def process_stocks():
+    print("Creating strains.txt")
+    input = open("users.txt", "r")
+    userdict = dict()
+    for line in input:
+        if line.split("\t")[0] == 'Username':
+            continue
+        line = line.strip()
+        if 'CORRECT' not in line.split("\t")[-1]:
+            userdict[line.split("\t")[0]] = line.split("\t")[1]
+        else:
+            LOGGER.warning("Missing information for user %s", line.split("\t")[1])
+    input.close()
+    input = open("Stock.tsv", "r")
+    output = open("strains.txt", "w")
+    output.write("Name\tUsername\tSpecies\n")
+    for line in input:
+        if line.split("\t")[0] == 'StockID':
+            continue
+        line = line.strip()
+        field = line.split("\t")
+        strain = field[1]
+        if field[-2] == '0':
+            # LOGGER.warning("Stock %s is not active", strain)
+            continue
+        if strain == '(null)':
+            # LOGGER.warning("Stock ID %s name is null", field[0])
+            continue
+        userid = field[-1]
+        if userid in userdict:
+            output.write("%s\t%s\t%s\n" % (strain, userdict[userid], 'zebrafish'))
+            LOGGER.info(strain)
+        else:
+            LOGGER.critical("Could not find user %s", userid)
+    input.close()
+    output.close()
+
+
+def process_tanks():
+    print("Creating tanks.txt")
+    input = open("users.txt", "r")
+    userdict = dict()
+    for line in input:
+        if line.split("\t")[0] == 'Username':
+            continue
+        line = line.strip()
+        if 'CORRECT' not in line.split("\t")[-1]:
+            userdict[line.split("\t")[0]] = line.split("\t")[1]
+        else:
+            LOGGER.warning("Missing information for user %s", line.split("\t")[1])
+    input.close()
+    input = open("Unit.tsv", "r")
+    output = open("tanks.txt", "w")
+    output.write("StockID\tName\tUsername\tAmount\t# males\t# females\tBirthdate\n")
+    for line in input:
+        if line.split("\t")[0] == 'Name':
+            continue
+        line = line.strip()
+        field = line.split("\t")
+        tank = field[0]
+        if field[1] == '0':
+            # LOGGER.warning("Tank %s is not active", tank)
+            continue
+        if tank == '(null)':
+            # LOGGER.warning("Tank name is null")
+            continue
+        userid = field[-1]
+        maleid = field[-2]
+        femaleid = field[-3]
+        if maleid == '(null)' and femaleid == '(null)':
+            LOGGER.warning("No stock ID for tank %s", tank)
+            continue
+        if userid in userdict:
+            birth = field[2].split(' ')[0]
+            output.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (femaleid, tank, userdict[userid],
+                                                           field[5] + field[6],
+                                                           field[5], field[6], birth))
+            LOGGER.info(tank)
+        else:
+            LOGGER.critical("Could not find user %s", userid)
+    input.close()
+    output.close()
+
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser(
+        description='Create new PyRAT files')
+    PARSER.add_argument('--verbose', action='store_true',
+                        dest='VERBOSE', default=False,
+                        help='Turn on verbose output')
+    PARSER.add_argument('--debug', action='store_true',
+                        dest='DEBUG', default=False,
+                        help='Turn on debug output')
+    ARG = PARSER.parse_args()
+
+    LOGGER = colorlog.getLogger()
+    if ARG.DEBUG:
+        LOGGER.setLevel(colorlog.colorlog.logging.DEBUG)
+    elif ARG.VERBOSE:
+        LOGGER.setLevel(colorlog.colorlog.logging.INFO)
+    else:
+        LOGGER.setLevel(colorlog.colorlog.logging.WARNING)
+    HANDLER = colorlog.StreamHandler()
+    HANDLER.setFormatter(colorlog.ColoredFormatter())
+    LOGGER.addHandler(HANDLER)
+
+    CONFIG = call_responder('config', 'config/rest_services')['config']
+    process_users()
+    process_stocks()
+    process_tanks()
+    sys.exit(0)
